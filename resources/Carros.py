d9fc0e3d948c73aae1397ac791d0e734c6d84681
reis071 <reiscorreia071@gmail.com>
2024-07-18 13:22:54 -0300

    add metho http put

--- resources/Carros.py
+++ resources/Carros.py
@@ -34,3 +34,29 @@ class Carro(Resource):
         
         lista_carros.append(novo_carro)
         return novo_carro,200
+    
+    def put(self,id_carro):
+        argumento = reqparse.RequestParser()
+        argumento.add_argument('nome')
+        argumento.add_argument('ano')
+        valor = argumento.parse_args()
+        
+        for carro in lista_carros:
+            if carro['id_carro'] == id_carro:
+                novo_carro = {'id_carro':id_carro,**valor}
+                carro.update(novo_carro)
+                return novo_carro,200
+  
+        argumento = reqparse.RequestParser()
+        argumento.add_argument('nome')
+        argumento.add_argument('ano')
+        
+        valor = argumento.parse_args()
+        carro_novo = {'id_carro':id_carro,**valor}
+        lista_carros.append(carro_novo)
+        return carro_novo,201 #criado
+    
+    def delete(self,id_carro):
+        global lista_carros
+        lista_carros = [carro for carro in lista_carros if carro['id_carro'] != id_carro]
+        return {"message":"carro deleted"}
